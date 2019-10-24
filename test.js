4bfea5983696ffb4ee9bdda35a4b7517d1b3efc6
Cameron Castells <CameronCastells@gmail.com>
2019-10-23 21:35:05 -0600

    testing get

--- test.js
+++ test.js
@@ -8,9 +8,18 @@ let headers = {
   Authorization:
     "Basic " + token
 };
+let response;
+
 request.get(
-  "https://ineomobility.atlassian.net/rest/api/2/search?jql=project=MOV", { headers }, (e, r, b) => {
-    console.log(r.statusCode);
+  `https://ineomobility.atlassian.net/rest/api/2/search?jql=project = "MOV" AND sprint in openSprints() AND Sprint not in closedSprints()&maxResults=150&startAt=0`, { headers }, (e, r, b) => {
+    let body = JSON.parse(b);
+    let issues = [];
+    body.issues.forEach(issue => {
+      let sprint = issue.fields.customfield_10113;
+      sprint = (sprint + '').match(/Sprint .{2}/g)[0].replace('Sprint ','');
+      issues.push({ sprint: sprint,key: issue.key, points: issue.fields.customfield_10115, status: issue.fields.status.name, summary: issue.fields.summary});
+    });
+    console.log(issues);
   }
 );
 
